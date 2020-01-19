7d4004d75b7435c12cb5a9710adf667e7941dce1
Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
2020-01-19 10:58:25 -0700

    spi: Update ZynqMP and Versal QSPI controller driver
    commit  5d37ab2cf69a5d4367f49fca798d78e4be66a43b from
    https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v5.10
    
    Updated ZynqMP and Versal QSPI controller driver to
     - Add support to enable data stripe in dual parallel mode.
     - Add support to select appropriate flash in stacked mode.
     - Set appropriate tapdelay values.
     - Add support for IO mode.
    
    Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
    Signed-off-by: Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>
    Signed-off-by: Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: P L Sai Krishna <lakshmis@xilinx.com>
    Signed-off-by: Ranjit Waghmode <ranjit.waghmode@xilinx.com>
    Signed-off-by: Tejas Prajapati Rameshchandra <tejas.prajapati.rameshchandra@xilinx.com>
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    State: pending
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- include/linux/spi/spi-mem.h
+++ include/linux/spi/spi-mem.h
@@ -347,6 +347,7 @@ bool spi_mem_dtr_supports_op(struct spi_mem *mem,
 
 int spi_mem_adjust_op_size(struct spi_mem *mem, struct spi_mem_op *op);
 
+bool update_stripe(const u8 opcode);
 bool spi_mem_supports_op(struct spi_mem *mem,
 			 const struct spi_mem_op *op);
 
