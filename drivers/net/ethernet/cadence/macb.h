05999fa8c271b382da365711c5da3ac1780323c6
Harini Katakam <harini.katakam@xilinx.com>
2020-01-31 21:07:03 +0530

    net: macb: Add PCS caps mask and enable autoneg
    commit  7b555c091331e3bd0a9ac78164aa76e95d6af4f2 from
    https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v5.10
    
    For SGMII, PCS auto-negotiation in PCS control register
    should be enabled. Add PCS caps mask and do the same.
    
    Signed-off-by: Harini Katakam <harini.katakam@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    State: pending
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- drivers/net/ethernet/cadence/macb.h
+++ drivers/net/ethernet/cadence/macb.h
@@ -492,6 +492,10 @@
 #define GEM_PCSAUTONEG_OFFSET			12
 #define GEM_PCSAUTONEG_SIZE			1
 
+/* Bitfields in PCSCNTRL */
+#define GEM_PCSAUTONEG_OFFSET			12
+#define GEM_PCSAUTONEG_SIZE			1
+
 /* Bitfields in DCFG1. */
 #define GEM_IRQCOR_OFFSET			23
 #define GEM_IRQCOR_SIZE				1
